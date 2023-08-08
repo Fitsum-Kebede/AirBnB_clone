c0cf8eede12388a27f0bbd64d0be8c8630daec1b
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-08-08 23:24:53 +0200

    BaseModel that defines all common attributes/methods for other classes

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,39 @@
 #!/usr/bin/python3
+"""This module define BaseModel class which defines all
+common attrbute and methods for subclasses.
+"""
+import datetime
+from uuid import uuid4
 
-from datetime import datetime
 
-today = datetime.today()
+class BaseModel:
+    """BaseModel class  definition"""
+    def __init__(self):
+        """Instatiate the BaseModel instance with unique id,
+        the date the instance or object is created and the date
+        at the which the instance or object was modified or updated.
+        """
+        self.id = str(uuid4())
+        self.created_at = datetime.datetime.today()
+        self.updated_at = datetime.datetime.today()
 
-print(today)
+    def __str__(self):
+        """Returns the string representation of the BaseModel"""
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                                      self.id, self.__dict__))
+
+    def save(self):
+        """Sets the date at which the BaseModel object has been updated"""
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """Returns BaseModel dictionary"""
+        model_dict = {}
+        self.__dict__["__class__"] = self.__class__.__name__
+        for key, value in self.__dict__.items():
+            if key == "created_at":
+                value = value.isoformat()
+            if key == "updated_at":
+                value = value.isoformat()
+            model_dict[key] = value
+        return (model_dict)
